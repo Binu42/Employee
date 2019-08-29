63c3595256b87482c8115243e0da37fe10969a36
Binu <kbinu42@gmail.com>
2019-08-29 07:34:12 +0530

    change in responsiveness

--- helper/hbs.js
+++ helper/hbs.js
@@ -1,9 +1,9 @@
 const moment = require('moment');
 
-// helper functions to truncate description to 120 characters and format date using moment package
+// helper functions to truncate description to 100 characters and format date using moment package
 module.exports = {
     truncate: function (str){
-        return str.substring(0, 120);
+        return str.substring(0, 100);
     },
 
     formatDate: function (date, format) {
